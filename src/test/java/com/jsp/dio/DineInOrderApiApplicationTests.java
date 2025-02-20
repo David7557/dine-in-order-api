8ef29758b0951631c49e278b86f6715afcb7e411
David7557 <davidkumar7557@gmail.com>
2025-02-20 07:20:39 +0530

    dine-in-order-api project initiated

---
+++ src/test/java/com/jsp/dio/DineInOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
